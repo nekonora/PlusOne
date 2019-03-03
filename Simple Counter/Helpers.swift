40096e40cf2fd78b2168dfee22e6038615cfae7d
Filippo Zaffoni <registration.fi.za@outlook.com>
2019-03-03 15:27:19 +0100

    Bug fixes

---
+++ Simple Counter/Helpers.swift
@@ -0,0 +1,18 @@
+//
+//  Helpers.swift
+//  Simple Counter
+//
+//  Created by Filippo Zaffoni on 03/03/2019.
+//  Copyright © 2019 Filippo Zaffoni. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class modelValidator {
+	
+	func checkValidityOf(countersList: [CounterStruct]) {
+		
+	}
+	
+}
