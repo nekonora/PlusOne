fd81706cebc8cfc5fdc65f07eeb2b8a345dca5f5
Filippo Zaffoni <registration.fi.za@outlook.com>
2019-05-23 22:59:57 +0200

    Completed CollectionView’s computed sizes to center cells.

--- Simple Counter/Models/CountersLayoutType.swift
+++ Simple Counter/Models/CountersLayoutType.swift
@@ -15,23 +15,40 @@ enum CountersLayoutType {
 	
 	// MARK: - Properties
 	static var small: UICollectionViewFlowLayout {
+		let screenWidth 		= UIScreen.main.bounds.width
+		let cellWidth: CGFloat 	= 176.0
+		
+		let maxNumberOfCellsPerRow = Int((screenWidth / cellWidth).rounded())
+		
+		let horizontalWhiteSpace = screenWidth - ( CGFloat(maxNumberOfCellsPerRow) * cellWidth )
+		let sideInset = (horizontalWhiteSpace / CGFloat( maxNumberOfCellsPerRow + 4 )).rounded()
+		
+		
 		let _flowLayout 					= UICollectionViewFlowLayout()
-		_flowLayout.itemSize 				= CGSize(width: 176, height: 186)
-		_flowLayout.sectionInset 			= UIEdgeInsets(top: 10, left: 5, bottom: 20, right: 5)
+		_flowLayout.itemSize 				= CGSize(width: cellWidth, height: 186)
+		_flowLayout.sectionInset 			= UIEdgeInsets(top: 10, left: sideInset * 2, bottom: 20, right: sideInset * 2)
 		_flowLayout.scrollDirection 		= UICollectionView.ScrollDirection.vertical
-		_flowLayout.minimumInteritemSpacing = 10.0
-		_flowLayout.minimumLineSpacing		= 15.0
+		_flowLayout.minimumInteritemSpacing = sideInset
+		_flowLayout.minimumLineSpacing		= sideInset * 2
 		_flowLayout.headerReferenceSize 	= CGSize(width: 100, height: 50)
 		return _flowLayout
 	}
 	
 	static var big: UICollectionViewFlowLayout {
+		let screenWidth 		= UIScreen.main.bounds.width
+		let cellWidth: CGFloat 	= 300
+		
+		let maxNumberOfCellsPerRow = Int((screenWidth / cellWidth).rounded())
+		
+		let horizontalWhiteSpace = screenWidth - ( CGFloat(maxNumberOfCellsPerRow) * cellWidth )
+		let sideInset = (horizontalWhiteSpace / CGFloat( maxNumberOfCellsPerRow + 4 )).rounded()
+		
 		let _flowLayout 					= UICollectionViewFlowLayout()
-		_flowLayout.itemSize 				= CGSize(width: 300, height: 186)
-		_flowLayout.sectionInset 			= UIEdgeInsets(top: 10, left: 10, bottom: 20, right: 10)
+		_flowLayout.itemSize 				= CGSize(width: cellWidth, height: 186)
+		_flowLayout.sectionInset 			= UIEdgeInsets(top: 10, left: sideInset * 2, bottom: 20, right: sideInset * 2)
 		_flowLayout.scrollDirection 		= UICollectionView.ScrollDirection.vertical
-		_flowLayout.minimumInteritemSpacing = 10.0
-		_flowLayout.minimumLineSpacing		= 15.0
+		_flowLayout.minimumInteritemSpacing = sideInset
+		_flowLayout.minimumLineSpacing		= sideInset * 2
 		_flowLayout.headerReferenceSize 	= CGSize(width: 100, height: 50)
 		return _flowLayout
 	}
