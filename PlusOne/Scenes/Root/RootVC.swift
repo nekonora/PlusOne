68813c8ebcd92f4ec6fa3a6ba1ff4b9b68db2fd4
Filippo Zaffoni <registration.fi.za@outlook.com>
2019-09-07 17:22:32 +0200

    New project structure

---
+++ PlusOne/Scenes/Root/RootVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlusOne
+//
+//  Created by Filippo Zaffoni on 07/09/2019.
+//  Copyright © 2019 Filippo Zaffoni. All rights reserved.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlusOneTests/PlusOneTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlusOneTests.swift
+//  PlusOneTests
+//
+//  Created by Filippo Zaffoni on 07/09/2019.
+//  Copyright © 2019 Filippo Zaffoni. All rights reserved.
+//
+
+import XCTest
+@testable import PlusOne
+
+class PlusOneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
